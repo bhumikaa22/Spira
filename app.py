7eed662d92fb7ca7d35b670b3ac9aeff2fc6b867
bhumika_w <bhumikawahane@gmail.com>
2025-06-12 18:27:30 +0530

    Fix project structure and link static files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify # type: ignore
 from flask_cors import CORS
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 import os
 import requests
 
