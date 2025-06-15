d1dd9214de856c2ffdda0e503859cdc01bfd2658
bhumika_w <bhumikawahane@gmail.com>
2025-06-15 22:41:15 +0530

    fix: Use correct download method in build script for new library version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 pip install -r requirements.txt
-python -c "import language_tool_python; language_tool_python.download_lt()"
+python -c "import language_tool_python; tool = language_tool_python.LanguageTool('en-US')"
